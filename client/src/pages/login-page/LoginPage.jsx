6f942e0d8fa246d4f63160e5444e573303ee1a4b
DeZoST <zosted62400@gmail.com>
2024-05-25 07:08:37 +0200

    Started login page

--- client/src/pages/login-page/LoginPage.jsx
+++ client/src/pages/login-page/LoginPage.jsx
@@ -1,6 +1,17 @@
+import styles from "./LoginPage.module.css"
+import Button from "../../components/button/Button"
+import Switch from "../../components/switch/Switch"
+import Title from "../../components/title/Title"
+
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+    <section className={`${styles.loginPage}`}>
+      <div className={"container"}>
+        <Switch label="Mode Modérateur" isChecked={false} />
+        <Title level={1} title="Connexion Pixel Perfect" />
+        <Button logo="/images/microsoft-logo.png" text="Se connecter à Microsoft" className={`${styles.button}`} />
+      </div>
+    </section>
   )
 }
 
