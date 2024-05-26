01de2593681aeafe3013313dcb8a1a384aa832d7
DeZoST <zosted62400@gmail.com>
2024-05-27 01:52:42 +0200

    Removed the Link usage to a Pop-Up

--- client/src/pages/login-page/components/user-login/UserContent.jsx
+++ client/src/pages/login-page/components/user-login/UserContent.jsx
@@ -1,18 +1,21 @@
 import styles from "./UserContent.module.css"
 import Button from "../../../../components/button/Button"
-import {Link} from "react-router-dom"
 
 const UserContent = () => {
     const loginUrl = `https://login.microsoftonline.com/consumers/oauth2/v2.0/authorize?client_id=${import.meta.env.VITE_CLIENT_ID}&response_type=code&redirect_uri=${import.meta.env.VITE_REDIRECT_URI}&scope=XboxLive.signin`
+
+    const handleLoginClick = () => {
+        window.open(loginUrl, "Microsoft Login", "width=600,height=800")
+    }
+
     return (
         <div className={`${styles.buttonContainer}`}>
-            <Link to={loginUrl}>
-                <Button
-                    logo="/images/microsoft-logo.png"
-                    text="Se connecter à Microsoft"
-                    className={`${styles.button}`}
-                />
-            </Link>
+            <Button
+                logo="/images/microsoft-logo.png"
+                text="Se connecter à Microsoft"
+                className={`${styles.button}`}
+                onClick={handleLoginClick}
+            />
         </div>
     )
 }
