1891d27c61c73be7a48a916701aeb572eb2c9797
Baptiste Moureaux <batoch3@hotmail.fr>
2024-05-28 00:36:39 +0200

    get minecraft informations from microsoft account

---
+++ server/src/controllers/authController.js
@@ -0,0 +1,109 @@
+export async function authenticateUser(req) {
+    const code = req.query.code
+    const microsoftToken = await getMicrosoftToken(code)
+
+    if (microsoftToken) {
+        const {uhs, xblToken} = await getXboxLiveToken(microsoftToken)
+        const xblXTSToken = await getXboxLiveXSTS(uhs, xblToken)
+        const mojangToken = await getMojangToken(uhs, xblXTSToken)
+        const mojangProfile = await getMojangProfile(mojangToken)
+
+        return mojangProfile
+    }
+
+    return null
+}
+
+async function getMicrosoftToken(code) {
+    const query = new URLSearchParams({
+        client_id: process.env.MICROSOFT_CLIENT_ID,
+        client_secret: process.env.MICROSOFT_CLIENT_SECRET,
+        code: code,
+        redirect_uri: process.env.MICROSOFT_REDIRECT_URI,
+        grant_type: "authorization_code",
+    }).toString()
+
+    const microsoft = await fetch("https://login.live.com/oauth20_token.srf", {
+        method: "POST",
+        body: query,
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+    })
+    const data = await microsoft.json()
+
+    return data.access_token
+}
+
+async function getXboxLiveToken(microsoftToken) {
+    const xbx = await fetch("https://user.auth.xboxlive.com/user/authenticate", {
+        method: "POST",
+        body: JSON.stringify({
+            RelyingParty: "http://auth.xboxlive.com",
+            TokenType: "JWT",
+            Properties: {
+                AuthMethod: "RPS",
+                SiteName: "user.auth.xboxlive.com",
+                RpsTicket: "d=" + microsoftToken,
+            },
+        }),
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+    })
+    const data = await xbx.json()
+    const uhs = data.DisplayClaims.xui[0].uhs
+    const xblToken = data.Token
+
+    return {uhs, xblToken}
+}
+
+async function getXboxLiveXSTS(uhs, xblToken) {
+    const xblSTS = await fetch("https://xsts.auth.xboxlive.com/xsts/authorize", {
+        method: "POST",
+        body: JSON.stringify({
+            RelyingParty: "rp://api.minecraftservices.com/",
+            TokenType: "JWT",
+            Properties: {
+                UserTokens: [xblToken],
+                SandboxId: "RETAIL",
+            },
+        }),
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+    })
+    const data = await xblSTS.json()
+    if (data.XErr) {
+        throw new Error("Aucun compte mojang n'est associé à ce compte Microsoft.")
+    }
+    return data.Token
+}
+
+async function getMojangToken(uhs, xblXTSToken) {
+    const mojang = await fetch("https://api.minecraftservices.com/authentication/login_with_xbox", {
+        method: "POST",
+        body: JSON.stringify({
+            identityToken: `XBL3.0 x=${uhs};${xblXTSToken}`,
+        }),
+        headers: {
+            "Content-Type": "application/json",
+        },
+    })
+    const mojangData = await mojang.json()
+
+    return mojangData.access_token
+}
+
+async function getMojangProfile(mojangToken) {
+    const profile = await fetch("https://api.minecraftservices.com/minecraft/profile", {
+        headers: {
+            Authorization: `Bearer ${mojangToken}`,
+        },
+    })
+    const profileData = await profile.json()
+
+    return profileData
+}
--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,29 +1,15 @@
 import express from "express"
+import {authenticateUser} from "../controllers/authController.js"
 
 const router = express.Router()
 
 router.get("/callback", async (req, res) => {
-    const code = req.query.code
-    const query = new URLSearchParams({
-        client_id: process.env.MICROSOFT_CLIENT_ID,
-        client_secret: process.env.MICROSOFT_CLIENT_SECRET,
-        code: code,
-        redirect_uri: process.env.MICROSOFT_REDIRECT_URI,
-        grant_type: "authorization_code",
-    }).toString()
-
-    const url = "https://login.microsoftonline.com/consumers/oauth2/v2.0/token"
-
-    const resp = await fetch(url, {
-        method: "POST",
-        body: query,
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-        },
-    })
-    const data = await resp.json()
-    console.log(data)
-    res.json()
+    try {
+        const data = await authenticateUser(req)
+        return res.json(data)
+    } catch (error) {
+        return res.status(500).json({error: error.message, stackTrace: error.stack})
+    }
 })
 
 export default router
