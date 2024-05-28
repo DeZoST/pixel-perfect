85773117cf0fae0fad426e9eb2bd6d27757f68a7
DeZoST <zosted62400@gmail.com>
2024-05-28 04:51:13 +0200

    Made a simple GamePage to see redirecting

--- client/src/pages/game-page/UserGamePage.jsx
+++ client/src/pages/game-page/UserGamePage.jsx
@@ -1,5 +1,22 @@
+import styles from "./UserGamePage.module.css"
+import ButtonDisconnect from "../../components/button-disconnect/ButtonDisconnect"
+import Title from "../../components/title/Title"
+
 const UserGamePage = () => {
-    return <div>UserGamePage</div>
+    return (
+        <section className={`${styles.userGamePage}`}>
+            <div className={`${styles.Container} container`}>
+                <header className={`${styles.header} animate__animated animate__fadeInRight`}>
+                    <ButtonDisconnect className={`${styles.button} animate__animated animate__fadeInRight`} />
+                </header>
+                <Title
+                    level={1}
+                    title="La partie commence bientôt"
+                    className={`${styles.title} animate__animated animate__fadeInDownBig`}
+                />
+            </div>
+        </section>
+    )
 }
 
 export default UserGamePage
