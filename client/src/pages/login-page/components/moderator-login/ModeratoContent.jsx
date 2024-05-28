a5efd90f9e9ece87efbaa31026526d72c056f028
DeZoST <zosted62400@gmail.com>
2024-05-28 04:49:50 +0200

    Added the Front-End to verify password

--- client/src/pages/login-page/components/moderator-login/ModeratoContent.jsx
+++ client/src/pages/login-page/components/moderator-login/ModeratoContent.jsx
@@ -2,10 +2,12 @@ import Button from "../../../../components/button/Button"
 import styles from "./ModeratorContent.module.css"
 import {useState} from "react"
 import PropTypes from "prop-types"
+import {useNavigate} from "react-router-dom"
 
 const ModeratorContent = ({className}) => {
     const [code, setCode] = useState("")
     const [_, setError] = useState("")
+    const navigate = useNavigate()
 
     const handleChange = e => {
         setCode(e.target.value)
@@ -16,9 +18,20 @@ const ModeratorContent = ({className}) => {
         setError("")
 
         try {
-            const response = await fetch("/api/pourLinstantRien", {
+            const response = await fetch("/api/verify-admin-code", {
                 method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({code}),
             })
+
+            if (response.ok) {
+                navigate("/admin-game")
+            } else {
+                const data = await response.json()
+                setError(data.message || "Le mot de passe est pas bon")
+            }
         } catch (error) {
             setError("Une erreur s'est produite quetpart")
         }
@@ -29,7 +42,7 @@ const ModeratorContent = ({className}) => {
             <div className={styles.codeContainer}>
                 <h2 className={styles.codeTitle}>Code du Modérateur :</h2>
                 <input
-                    type="text"
+                    type="password"
                     id="code"
                     value={code}
                     onChange={handleChange}
@@ -37,6 +50,7 @@ const ModeratorContent = ({className}) => {
                     placeholder="Insérer ici le code..."
                     className={styles.codeInput}
                 ></input>
+                {_ && <p className={styles.error}>{_}</p>}
             </div>
             <div className={styles.buttonContainer}>
                 <Button type="submit" text="Prochaine étape" className={styles.button}></Button>
