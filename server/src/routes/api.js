2f173352bbaab082c1566e1d4e880614228e11e9
Baptiste Moureaux <batoch3@hotmail.fr>
2024-05-31 00:44:47 +0200

    add routes TODO

--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -6,4 +6,12 @@ router.post("/test", (req, res) => {
     res.json({message: "Hello, World!"})
 })
 
+// vote route
+
+// change waitingSentence route
+
+// toggle pause route
+
+// start game route
+
 export default router
--- server/src/routes/websocket.js
+++ server/src/routes/websocket.js
@@ -1,1 +1,39 @@
-// empty for the moment
+/* empty for the moment
+
+gameState route :
+{
+    isStarted: false,
+    isFinished: false,
+    isPaused: false,
+    secondsRemaining: 0,
+    currentTeam: ?,
+    waitingSentence: "",
+    IF ITS A MODERATOR : 
+    currentPoints:{
+        red: 0,
+        pink: 0,
+        lime: 0,
+        green: 0,
+        blue: 0,
+        yellow: 0,
+    }
+}
+
+leaderboard route :
+[
+    team: ?,
+    teamNumber: ?,
+    rank: ?,
+    points: ?,
+    wools: {
+        red: ?,
+        pink: ?,
+        lime: ?,
+        green: ?,
+        blue: ?,
+        yellow: ?,
+    }
+
+]
+
+*/
