51944d53c25b180bef89965800b1d2781dba4e52
DeZoST <zosted62400@gmail.com>
2024-06-04 05:24:50 +0200

    Implemented socket.io with websocket

--- client/src/components/waiting-message/WaitingMessage.jsx
+++ client/src/components/waiting-message/WaitingMessage.jsx
@@ -1,15 +1,28 @@
 import PropTypes from "prop-types"
+import styles from "./WaitingMessage.module.css"
+import {io} from "socket.io-client"
+import {useEffect, useState} from "react"
 
-const WaitingMessage = ({message}) => {
-    return (
-        <div className="waiting-message">
-            <p>{message}</p>
-        </div>
-    )
+const socket = io("http://localhost:3000")
+
+const WaitingMessage = ({initialMessage}) => {
+    const [message, setMessage] = useState(initialMessage)
+
+    useEffect(() => {
+        socket.on("waitingSentenceUpdated", newMessage => {
+            setMessage(newMessage)
+        })
+
+        return () => {
+            socket.off("waitingSentenceUpdated")
+        }
+    }, [])
+
+    return <p className={styles.waitingMessage}>{message}</p>
 }
 
 WaitingMessage.propTypes = {
-    message: PropTypes.string.isRequired,
+    initialMessage: PropTypes.string.isRequired,
 }
 
 export default WaitingMessage
