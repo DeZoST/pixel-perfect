2c2431f85cf68ab31f4c456ee6153083b133c2e5
DeZoST <zosted62400@gmail.com>
2024-06-07 06:08:30 +0200

    Started implementing game logic

--- client/src/pages/game-page/GamePage.jsx
+++ client/src/pages/game-page/GamePage.jsx
@@ -2,6 +2,7 @@ import styles from "./GamePage.module.css"
 import {useEffect, useState} from "react"
 import GameWaiting from "./components/gameWaiting/GameWaiting"
 import GamePlaying from "./components/gamePlaying/GamePlaying"
+import GameScoreboard from "./components/gameScoreboard/GameScoreboard"
 import Logo from "../../components/logo/Logo"
 import {io} from "socket.io-client"
 import {useAuth} from "../../hooks/useAuth"
@@ -10,7 +11,14 @@ import PropTypes from "prop-types"
 const UserGamePage = () => {
     const [game, setGame] = useState({})
     const [votes, setVotes] = useState({})
+    const [players, setPlayers] = useState([])
+    const [teams, setTeams] = useState([])
     const {user} = useAuth()
+
+    const [isStarted, setIsStarted] = useState(false)
+    const [isPaused, setIsPaused] = useState(false)
+    const [isFinished, setIsFinished] = useState(false)
+    const [currentTeam, setCurrentTeam] = useState({})
 
     useEffect(() => {
         const socket = io("localhost:3000", {
@@ -22,6 +30,16 @@ const UserGamePage = () => {
         socket.on("game.listen", data => {
             console.log(data)
             setGame(data)
+            setIsStarted(data.isStarted === 1)
+            setIsPaused(data.isPaused === 1)
+            setIsFinished(data.isFinished === 1)
+
+            if (data.currentTeamId) {
+                const team = teams.find(team => team.ID === data.currentTeamId)
+                if (team) {
+                    setCurrentTeam(team)
+                }
+            }
         })
 
         socket.on("vote.listen", data => {
@@ -29,16 +47,46 @@ const UserGamePage = () => {
             setVotes(data)
         })
 
+        socket.on("players.listen", data => {
+            console.log(data)
+            setPlayers(data)
+        })
+
         return () => {
             socket.disconnect()
         }
-    }, [])
+    }, [teams, user.jwt])
+
+    useEffect(() => {
+        async function fetchTeams() {
+            const response = await fetch(`${import.meta.env.VITE_SERVER_URL}/api/teams`, {
+                headers: {
+                    Authorization: `Bearer ${user.jwt}`,
+                },
+            })
+            const teamData = await response.json()
+            setTeams(teamData)
+        }
+        fetchTeams()
+    }, [user.jwt])
 
     return (
         <section className={`${styles.userGamePage}`}>
             <Logo className={`${styles.logo}`} />
             <div className={`${styles.Container} container`}>
-                <GameWaiting game={game} />
+                {isFinished ? (
+                    <GameScoreboard game={game} />
+                ) : isStarted ? (
+                    <GamePlaying
+                        game={game}
+                        votes={votes}
+                        players={players}
+                        currentTeam={currentTeam}
+                        isPaused={isPaused}
+                    />
+                ) : (
+                    <GameWaiting game={game} />
+                )}
             </div>
         </section>
     )
--- client/src/pages/game-page/components/gamePlaying/GamePlaying.jsx
+++ client/src/pages/game-page/components/gamePlaying/GamePlaying.jsx
@@ -1,35 +1,141 @@
+import {useEffect, useRef, useState} from "react"
 import Title from "../../../../components/title/Title"
 import Wool from "../../../../components/wool/Wool"
 import styles from "./GamePlaying.module.css"
+import PropTypes from "prop-types"
+import axios from "axios"
+import {useAuth} from "../../../../hooks/useAuth"
 
-const GamePlaying = () => {
+const GamePlaying = ({game, votes, players, currentTeam, isPaused}) => {
+    const videoRef = useRef(null)
+    const [selectedVote, setSelectedVote] = useState(null)
+    const {user} = useAuth()
+
+    useEffect(() => {
+        if (videoRef.current) {
+            videoRef.current.load()
+        }
+        setSelectedVote(null) // Reset selected vote when the team changes
+    }, [currentTeam])
+
+    const handleVote = async vote => {
+        if (isPaused) {
+            alert("La partie est en pause, vous ne pouvez pas voter.")
+            return
+        }
+
+        setSelectedVote(vote)
+
+        try {
+            const response = await axios.put(
+                `${import.meta.env.VITE_SERVER_URL}/api/game/vote`,
+                {
+                    wool: vote,
+                    teamId: currentTeam.ID,
+                    playerId: user.id,
+                },
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${user.jwt}`,
+                    },
+                },
+            )
+        } catch (error) {
+            console.error("Error submitting vote:", error.response || error.message)
+            alert(
+                `Erreur lors de la soumission du vote: ${error.response ? error.response.data.message : error.message}`,
+            )
+        }
+    }
+
     return (
         <section className={`${styles.gamePlayingPage}`}>
             <section className={`${styles.gamePlayingContainer} container`}>
                 <header className={`${styles.headerContainer}`}>
-                    <Title level={1} title="Votez la qualité de la vidéo" className={styles.title} />
+                    {isPaused ? (
+                        <Title level={1} title="La partie est en pause" className={styles.title} />
+                    ) : (
+                        <Title level={1} title="Votez la qualité de la vidéo" className={styles.title} />
+                    )}
+
                     <div className={`${styles.timerContainer}`}>
                         <div className={`${styles.timerLine}`}></div>
-                        <h2 className={`${styles.timerText}`}>X secondes restantes</h2>
+                        <h2 className={`${styles.timerText}`}>{game.secondsRemaining} secondes restantes</h2>
                     </div>
                 </header>
                 <div className={`${styles.mainGameContainer}`}>
                     <div className={`${styles.videoContainer}`}>
-                        <h2 className={`${styles.teamName}`}>Team Daniell</h2>
-                        <video className={`${styles.video}`} controls></video>
+                        <h2 className={`${styles.teamName}`}>Team {currentTeam.NAME}</h2>
+                        <video className={`${styles.video}`} ref={videoRef} autoPlay controls>
+                            <source
+                                src={`${import.meta.env.VITE_SERVER_URL}/uploads/${game.currentTeamId}.mp4`}
+                                type="video/mp4"
+                            />
+                        </video>
                     </div>
                     <div className={`${styles.woolsContainer}`}>
-                        <Wool number={1} rarity="Superpoop" color="red" />
-                        <Wool number={2} rarity="Poop" color="pink" />
-                        <Wool number={3} rarity="Good" color="lightgreen" />
-                        <Wool number={4} rarity="Very good" color="darkgreen" />
-                        <Wool number={5} rarity="Epic" color="blue" />
-                        <Wool number={6} rarity="Legendary" color="yellow" />
+                        <Wool
+                            number={1}
+                            rarity="Superpoop"
+                            color="red"
+                            onClick={() => handleVote(1)}
+                            selected={selectedVote === 1}
+                            dimmed={selectedVote !== null && selectedVote !== 1}
+                        />
+                        <Wool
+                            number={2}
+                            rarity="Poop"
+                            color="pink"
+                            onClick={() => handleVote(2)}
+                            selected={selectedVote === 2}
+                            dimmed={selectedVote !== null && selectedVote !== 2}
+                        />
+                        <Wool
+                            number={3}
+                            rarity="Good"
+                            color="lightgreen"
+                            onClick={() => handleVote(3)}
+                            selected={selectedVote === 3}
+                            dimmed={selectedVote !== null && selectedVote !== 3}
+                        />
+                        <Wool
+                            number={4}
+                            rarity="Very good"
+                            color="darkgreen"
+                            onClick={() => handleVote(4)}
+                            selected={selectedVote === 4}
+                            dimmed={selectedVote !== null && selectedVote !== 4}
+                        />
+                        <Wool
+                            number={5}
+                            rarity="Epic"
+                            color="blue"
+                            onClick={() => handleVote(5)}
+                            selected={selectedVote === 5}
+                            dimmed={selectedVote !== null && selectedVote !== 5}
+                        />
+                        <Wool
+                            number={6}
+                            rarity="Legendary"
+                            color="yellow"
+                            onClick={() => handleVote(6)}
+                            selected={selectedVote === 6}
+                            dimmed={selectedVote !== null && selectedVote !== 6}
+                        />
                     </div>
                 </div>
             </section>
         </section>
     )
+}
+
+GamePlaying.propTypes = {
+    game: PropTypes.object,
+    votes: PropTypes.object,
+    players: PropTypes.array,
+    currentTeam: PropTypes.object,
+    isPaused: PropTypes.bool,
 }
 
 export default GamePlaying
