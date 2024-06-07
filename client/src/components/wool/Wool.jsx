7fd827a94fd54f3bb1403b93a8505cf5ff6e5319
DeZoST <zosted62400@gmail.com>
2024-06-07 06:07:35 +0200

    Added wool styles when selected, not selected, hovered

--- client/src/components/wool/Wool.jsx
+++ client/src/components/wool/Wool.jsx
@@ -1,11 +1,37 @@
 import PropTypes from "prop-types"
 import styles from "./Wool.module.css"
+import {useState} from "react"
 
-const Wool = ({number, rarity, color}) => {
+const Wool = ({number, rarity, color, onClick, selected, dimmed}) => {
+    const [isHovered, setIsHovered] = useState(false)
+
+    const woolColors = {
+        red: "#5C1513", // Superpoop
+        pink: "#8B4D60", // Poop
+        lightgreen: "#3C660D", // Good
+        darkgreen: "#303E10", // Very good
+        blue: "#2C2D7C", // Epic
+        yellow: "#DAAC1F", // Legendary
+    }
+
+    const dropShadowStyle = {
+        filter: isHovered ? `drop-shadow(0 0 10px ${woolColors[color]})` : "",
+    }
+
     return (
-        <div className={`${styles.woolContainer}`}>
+        <div
+            className={`${styles.woolContainer}`}
+            onClick={onClick}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+        >
             <p className={`${styles.rarityText}`}>{rarity}</p>
-            <img className={`${styles.wool}`} src={`/images/wool-${color}.png`} alt="wool for voting" />
+            <img
+                className={`${styles.wool} ${selected ? styles.selected : ""} ${dimmed ? styles.dimmed : ""}`}
+                src={`/images/wool-${color}.png`}
+                alt="wool for voting"
+                style={dropShadowStyle}
+            />
         </div>
     )
 }
@@ -14,6 +40,9 @@ Wool.propTypes = {
     color: PropTypes.string.isRequired,
     number: PropTypes.number.isRequired,
     rarity: PropTypes.string.isRequired,
+    onClick: PropTypes.func.isRequired,
+    selected: PropTypes.bool.isRequired,
+    dimmed: PropTypes.bool.isRequired,
 }
 
 export default Wool
